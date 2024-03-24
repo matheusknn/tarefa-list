714abe1a36d1b8c8ecd0b526be22c5912162e015
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-03-24 09:29:00 -0300

    first commit

---
+++ src/main/java/br/com/kenji/tarefalist/TarefalistApplication.java
@@ -0,0 +1,13 @@
+package br.com.kenji.tarefalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefalistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kenji/tarefalist/TarefalistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kenji.tarefalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
