de5085437ede64404b10f1319d14b3054fa50780
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-03-25 21:22:25 -0300

    criar a página de cadastro de tarefas

--- src/main/java/br/com/kenji/tarefalist/controllers/TarefaController.java
+++ src/main/java/br/com/kenji/tarefalist/controllers/TarefaController.java
@@ -1,10 +1,13 @@
 package br.com.kenji.tarefalist.controllers;
 
+import br.com.kenji.tarefalist.models.Tarefa;
 import br.com.kenji.tarefalist.repositories.TarefaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Controller
@@ -20,4 +23,18 @@ public class TarefaController {
     public ModelAndView list() {
         return new ModelAndView("tarefa/lista", Map.of("tarefas", tarefaRepository.findAll()));
     }
+
+    @GetMapping("/create")
+    public ModelAndView create() {
+        return new ModelAndView("tarefa/form", Map.of("tarefa", new Tarefa()));
+    }
+
+    @PostMapping("/create")
+    public String create(Tarefa tarefa) {
+        var dataAtual = LocalDateTime.now();
+        tarefa.setCriacao(dataAtual);
+        tarefaRepository.save(tarefa);
+        return "redirect:/";
+    }
+
 }
